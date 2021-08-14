24005f9b59221fe2f9c75d83b3b9457ee7934668
Rio Osanai <osanai.rio@gmail.com>
2021-08-14 21:58:43 +0900

    create jsx

--- src/App.js
+++ src/App.js
@@ -1,8 +1,24 @@
+import React from 'react';
+
+// inputタグのラベルとして作用する
+// 例えばチェックボックスのID値をラベルのforに設定することでラベルで指定した文字をクリックしてもチェックできる
+
 function App() {
   return (
-    <h1>
-      hello!React!
-    </h1>
+
+    <React.Fragment>
+      <h1>
+        hello!React!
+      </h1>
+
+      <label htmlFor="bar">bar</label>
+
+      <input type="text" onClick={() => { console.log('i clicled button') }} />
+      <button text="button" onClick={() => { console.log('i clicled button') }}>
+        chiken
+      </button>
+
+    </React.Fragment>
   );
 }
 
